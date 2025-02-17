d3959c45bfbe5fc06a55717d6fbaec16721ddd0c
Samwoker <149298507+Samwoker@users.noreply.github.com>
2025-02-16 21:59:54 -0900

    feed api

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,22 +3,36 @@ const userRouter = express.Router();
 const ConnectionRequest = require("../models/connection");
 
 const { userAuth } = require("../utils/auth");
+
+const feedData = "firstName lastName about age gender photoUrl hobbies";
 userRouter.get("/connection", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
 
     const connections = await ConnectionRequest.find({
       $and: [{ toUserId: loggedInUser._id }, { status: "connect" }],
-    }).populate("fromUserId", [
-      "firstName",
-      "lastName",
-      "about",
-      "age",
-      "gender",
-      "photoUrl",
-      "hobbies",
-    ]);
-    res.json({connections: connections})
+    }).select("fromUserId", feedData);
+    res.json({ connections: connections });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+userRouter.get("/feed?page&limit", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    const feeds = await ConnectionRequest.find({
+      $or: [
+        { toUserId: loggedInUser._id, status: "confirm" },
+        { fromUserId: loggedInUser._id, status: "confirm" },
+      ],
+    })
+      .skip(skip)
+      .limit(limit)
+      .select("fromUserId toUserId", feedData);
   } catch (err) {
     res.status(400).json({ message: err.message });
   }
